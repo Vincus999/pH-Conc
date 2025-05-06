e2e6c18c1ada412e24e6cf84ed47b32657b65379
Vincus999 <92683593+Vincus999@users.noreply.github.com>
2025-05-07 00:31:50 +0200

    Add files via upload

---
+++ concentration.py
@@ -0,0 +1,24 @@
+
+def concentration():
+   try:
+       pH = float(input("Please give me the value of the pH: "))
+       if pH < 0 or pH > 14:
+           print("Please give me the value between 0 and 14")
+           return
+
+       if pH <= 7:
+           h_conc = 10 ** (-pH)
+           print(f"The concentration is:  {h_conc} mol/L")
+
+       else:
+           pOH = 14 - pH
+           oh_conc = 10 ** (-pOH)
+           print(f"The concentration is:  {oh_conc} mol/L")
+
+   except ValueError:
+       print("Please give me the value between 0 and 14")
+
+
+print(concentration())
+
+
